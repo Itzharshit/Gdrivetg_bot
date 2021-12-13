2fa2e32ff2007973ee0c274285c21bef9e3d7a2f
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-12-13 14:48:12 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -1,7 +1,7 @@
 
 
 # Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
-drive_folder_name = "GDriveUploaderBot"
+drive_folder_name = "Pyrogrammers Google Drive"
 
 
 # Enter Your Mega email And Pass (Required)
@@ -9,63 +9,34 @@ MEGA_EMAIL = "bearyan8@yandex.com"
 MEGA_PASSWORD = "bearyan8@yandex.com"
 
 
-START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .By using /auth \n\n For more info /help \n\n Third-Party Website \n Support Added /update \n\n For Bot Updates  \n <a href ='https://t.me/super_botz'>Join Channel</a>\nPlease Report Bugs  @Wizard09"
+START = " 𝗛𝗶𝗶 {}\n𝗜 𝗮𝗺 𝗚𝗼𝗼𝗴𝗹𝗲 𝗗𝗿𝗶𝘃𝗲 𝗨𝗽𝗹𝗼𝗮𝗱𝗲𝗿 𝗕𝗼𝘁 𝗰𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆 @pyrogrammers Please /auth To use me.Hit /help to get more information.I also support mega.nz,Openload links and dropbox link 🤫."
 
-HELP = """   <b>AUTHORISE BOT</b> 
-       Use  /auth Command Generate
-       Your Google Drive Token And 
-       Send It To Bot  
-<b> You Wanna Change Your Login 
-        Account ?</b> \n
-        You Can Use /revoke 
-        command            
-<b>What I Can Do With This Bot? </b>
-            You Can Upload Any Internet
-            Files On Your google
-            Drive Account.
-<b> Links Supported By Bot</b>
-            * Direct Links 
-            * Openload links [Max Speed 
-              500 KBps :(   ]
-            * Dropbox links 
-            *  Mega links
-            
-            + More On Its way:)
-                
-Bug Report @super_botz_support
+HELP = """
+Use /auth Command Generate Google drive api token and send it to bot.
+Use /revoke to logout from bot.            
+Use /help to get this message 😜.
         """
-DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
-OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow"
-PROCESSING = "Processing Your Request ...!!"
+DP_DOWNLOAD = "Dropbox Link !! ⏳ Downloading Started ..."
+OL_DOWNLOAD = "Openload Link !! ⏳ Downloading Started ..."
+PROCESSING = "Haah! Processing...!!"
 DOWN_TWO = True
-DOWNLOAD = "Downloading Started ..."
-DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow :("
-DOWN_COMPLETE = "Downloading complete !!"
-NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n @super_botz_support"
-REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @super_botz_support"
-AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n report At @super_botz_support"
+DOWNLOAD = "⏳ Downloading Started ..."
+DOWN_MEGA = "⏳ Downloading Started... "
+DOWN_COMPLETE = "✅ Downloading complete !!"
+NOT_AUTH = "You Are Not Authorised To Use me \n\nPlease Authorise Me Using /auth to continue further steps."
+REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise."
+AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A Raw Link."
+ALREADY_AUTH = "You Are Already Authorised !Hit /revoke to logout."
 AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
 UPLOADING = "Download Complete !! \n Uploading Your file"
-REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
+REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise. "
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At @Wizard09"
+AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me."
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
 
 
-UPDATE = """ <b> Update  on  27.07.2019</b>
-            * MEGA LINK added
-            * Error Handling Improved
-<b> Links Supported By Bot</b>
-            * Direct Links 
-            * Openload links [Max Speed 
-              500 KBps :(   ]
-            * Dropbox links 
-            *  Mega links (only files)
-            
-            + More are in way:)
-            Join @super_botz """
+UPDATE = """For updates of this bot join @Pyrogrammers """
